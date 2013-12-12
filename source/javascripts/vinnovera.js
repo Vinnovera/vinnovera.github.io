fc75c27beb6f92c489ef9e6992e224093704902d
tbleckert <tbleckert@gmail.com>
2013-12-12 14:02:52 +0100

    Vinnovera theme

---
+++ source/javascripts/vinnovera.js
@@ -0,0 +1,16 @@
+!function () {
+	
+	'use strict';
+	
+	window.addEvent('domready', function () {
+	
+		/*var header = document.id('header'),
+		    smallHeader = header.clone().addClass('small');
+			
+			console.log(header);
+			
+		header.grab(smallHeader, 'after');*/
+		
+	});
+	
+}();
