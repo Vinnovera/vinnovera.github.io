4290b3c51732356b1c6d277f3d1ef198994004f3
tbleckert <tbleckert@gmail.com>
2014-01-09 15:26:35 +0100

    Smooth scrolling

--- source/javascripts/vinnovera.js
+++ source/javascripts/vinnovera.js
@@ -4,8 +4,12 @@
 	
 	window.addEvent('domready', function () {
 	
-		var scroll, scrollUp = document.id('scrollUp');
+		var scroll, scrollUp = document.id('scrollUp'), bodyTween;
 		
+		// Body animation
+		bodyTween = new Fx.Scroll(window);
+		
+		// Show to top button
 		window.addEvent('scroll', function (e) {
 			scroll = document.documentElement.scrollTop || document.body.scrollTop;
 			
@@ -14,6 +18,22 @@
 			} else {
 				scrollUp.removeClass('visible');
 			}
+		});
+		
+		// Scroll to top
+		scrollUp.addEvent('click', function (e) {
+			e.stop();
+			
+			bodyTween.start(0, 0);
+		});
+		
+		document.body.addEvent('click:relay(nav > a)', function (e) {
+			e.stop();
+			
+			var hash = this.hash.substr(1),
+			    top  = document.id(hash).getPosition().y;
+			
+			bodyTween.start(0, top);
 		});
 		
 	});
