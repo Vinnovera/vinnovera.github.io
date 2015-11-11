0a06bed407f81e84208217e01b167d6e28fe5405
Macke <arcus.johansson@gmail.com>
2015-11-11 15:07:49 +0100

    Update

--- javascripts/sumo/main.js
+++ javascripts/sumo/main.js
@@ -1,56 +1,43 @@
-/*function () {
-		document.body.addEvent('click:relay(.sumo img)', function (e) {
-
-		e.preventDefault();
-
-		console.log('clicked');
-			
-		var copy = this.clone();
-		copy.set('id', 'fullscreen_image');
-		copy.inject(document.body);
-
-		copy.addEvent('load', function(e) {
-			var jso = new jsOverlay({
-				content: 'fullscreen_image',
-				usePushState: false,
-				scrollable: true
-			});
-		});
-	});
-
-}();*/
-
 $(document).ready(function() {
-
-  loadFancybox();
-  randomQuotes();
-
+	loadFancybox();
+	randomQuotes();
 });
 
 
 function randomQuotes() {
 
-  var element = document.getElementById("quotes");
-  
+	var quotes = [
+		["It was the most fucking awesome fun that I had in a long time. Kudos to everyone who attended!!", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["Spectacular! Insane amounts of fun was had!", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["It was awesome. I'm just mad I didn't win!", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["It was awesome, more people should have tried it!", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["Was very funny to watch.", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["Funny and unexpected.", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["It was great fun! Possibly there should be more than one ring at a time to make it move at a better pace.", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["Great fun to watch. Would like to attend next time.", "Nordic.js Mini-Sumo Robo Clash Attendee"],
+		["Cool!", "Nordic.js Mini-Sumo Robo Clash Attendee"]
+	];
 
-  var quotes = [
-    ["It was the most fucking awesome fun that I had in a long time. Kudos to everyone who attended!!", "random attendee"],
-    ["Spectacular! Insane amounts of fun was had!", "random attendee"],
-    ["It was awesome. I'm just mad I didn't win!", "random attendee"],
-    ["It was awesome, more people should have tried it!", "random attendee"]
-  ];
+	$("#quotes").html("<p class='quote'>"+ quotes[0][0] + "</p><footer>" + quotes[0][1] + "</footer>")
 
-  var random = quotes[Math.floor(Math.random() * quotes.length)];
+	var interval = setInterval(function() {
 
-    function quote() {
-    	element.innerHTML = "<p class='quote'>"+ random[0] + "</p><footer>" + random[1] + "</footer>";
+		var i = Math.round((Math.random()) * quotes.length);
 
-    }
-
-  quote();
-
+		if (i == quotes.length) {
+			--i;
+		}
+		$('#quotes').addClass('fadeOut');
+			var timer = setTimeout(function() {
+				$("#quotes p.quote").text(quotes[i][0]);
+				$("#quotes footer").text(quotes[i][1]);
+				$('#quotes').removeClass('fadeOut');
+			clearTimeout(timer);
+		}, 500);
+	}, 5000);
 };
 
+
 function loadFancybox() {
-  $(".fancybox").fancybox();
+	$(".fancybox").fancybox();
 };
