fa6d100d08e63eb254fce6e0cb6902f1885f9b38
alexandra <alexandra@vinnovera.se>
2015-11-17 13:31:53 +0100

    labs.js, lite filtrering

---
+++ src/javascripts/labs.js
@@ -0,0 +1,24 @@
+/**
+ * Created by sandra on 17/11/15.
+ */
+
+$(document).ready(function() {
+	initIsotop();
+	bindEvents();
+});
+
+function bindEvents() {
+	$('.labs-filter').on( 'click', 'button', onFilterButton);
+}
+
+function initIsotop() {
+	var $grid = $('.grid').isotope({
+		itemSelector: '.labs-item',
+		layoutMode: 'packery'
+	});
+}
+
+function onFilterButton(e) {
+	var filterValue = $(e.target).attr('data-filter');
+	$('.labs').isotope({ filter: filterValue });
+}
